c9096ffa6403f579da287e1172277665f21c2cfa
isilay-subasi <isil.subasi19@gmail.com>
2021-07-04 15:25:04 +0300

    Switch-Between-Activities

---
+++ app/src/main/java/com/example/switchbetweenactivities/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.switchbetweenactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startSecondActivity(View view) {
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+
+    }
+}
